aa173a35112a3de2c4aa40c65725d48a68498654
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-05-13 20:47:26 -0400

    Create ntfsfix.sh bash script to remove drive folders left behind after unmount with ntfs-3g in OS X

---
+++ ntfsfix.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Written by Nikolai Vazquez
+# Solution for removing drive folders left behind after unmount with ntfs-3g in OS X
+# Usage ./ntfsfix.sh [list of volumes]
+
+if [ $# -eq 0 ]; then
+	declare -a points=("share" "Windows 8.1")
+else
+	points="$@"
+fi
+
+for point in "${points[@]}"; do
+	if mount | grep "on $point" > /dev/null; then
+		echo "All is good, nothing to see here. \"$point\" is mounted Move along."
+	else
+		if diskutil list | grep "$point" > /dev/null; then
+			diskutil unmount "$point"
+			sudo find /Volumes -maxdepth 1 -name "$point*" -exec rm -rf {} \;
+		else
+			echo "\"$point\" is not a valid volume"
+		fi
+	fi
+done
