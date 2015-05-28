a964e3f4d46c996b0d6682c0406b10fff499d3a9
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-05-28 18:26:38 -0400

    Create catbin.sh bash script to print out contents of a specified file in $PATH

---
+++ catbin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Written by Nikolai Vazquez
+# Prints out contents of a specified file in your $PATH
+#
+# Usage:
+# ./catbin.sh PATH_FILE
+
+if [[ ! "$1" == "" ]]; then
+    cat $(which $1)
+fi
